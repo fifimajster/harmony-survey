1ead509b789b9aa4f75e784c4050dae6bc25cf8f
fifimajster <filipeyro@gmail.com>
2018-08-24 18:39:20 +0200

    better tone, refactor

--- plot_data.py
+++ plot_data.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 glide over to check out other frequencies
 click to form a chord
@@ -8,116 +9,91 @@ orange is the harmony in a chord
 import pickle
 
 import matplotlib.pyplot as plt
+import pyaudio
+import survey as sr
 from scipy.signal import savgol_filter
 from scipy.stats import rankdata
 
-import survey as sr
 
-
-# credit: stackoverflow.com/questions/10702942/note-synthesis-harmonics-violin-piano-guitar-bass-frequencies-midi
-violin_amps = [1.0, 0.286699025, 0.150079537, 0.042909002,
-            0.203797365, 0.229228698, 0.156931925,
-            0.115470898, 0.0, 0.097401803, 0.087653465,
-            0.052331036, 0.052922462, 0.038850593,
-            0.053554676, 0.053697434, 0.022270261,
-            0.013072562, 0.008585879, 0.005771505,
-            0.004343925, 0.002141371, 0.005343231,
-            0.000530244, 0.004711017, 0.009014153]
-
-piano_amps = [1.0, 0.399064778, 0.229404484, 0.151836061,
-            0.196754229, 0.093742264, 0.060871957,
-            0.138605419, 0.010535002, 0.071021868,
-            0.029954614, 0.051299684, 0.055948288,
-            0.066208224, 0.010067391, 0.00753679,
-            0.008196947, 0.012955577, 0.007316738,
-            0.006216476, 0.005116215, 0.006243983,
-            0.002860679, 0.002558108, 0.0, 0.001650392]
-
-
+# define plot
 plt.style.use('dark_background')
 fig, ax = plt.subplots()
 
 
-def redraw():
+data, p, stream, filename = sr.init()
+
+sorted_data = sorted(data.items())   # sorted by key, return a list of tuples
+xs, ys = zip(*sorted_data)      # unpack a list of pairs into two tuples
+ys = rankdata(ys) / len(ys)     # normalize ranking
+
+smooth = savgol_filter(ys, 15, 3)
+# smooth = savgol_filter(smooth, 21, 3)
+
+
+def draw():
     ax.set_ylim([0, 1])
-    notes = [2**(i/12) for i in range(1, 13)]
+    notes = [2 ** (i / 12) for i in range(1, 13)]
     for note in notes:
         ax.axvline(x=note, color='purple', linewidth=0.5)
+    ax.plot(xs, smooth, 'blue')
     fig.canvas.draw()
 
 
-def main():
-    # load data
-    print('\nchoose nick:')
-    filename = "data/%s.pickle" % input()
-    data = pickle.load(open(filename, "rb"))
-    print('data length: %d' % len(data))
-
-    sorted_data = sorted(data.items())   # sorted by key, return a list of tuples
-    xs, ys = zip(*sorted_data)      # unpack a list of pairs into two tuples
-    ys = rankdata(ys) / len(ys)     # normalize ranking
-
-    # print smoothed data
-    smooth = savgol_filter(ys, 15, 3)
-    # smooth = savgol_filter(smooth, 21, 3)
-    redraw()
-    ax.plot(xs, smooth, 'blue')
-
-    def harmony(ratio):
-        # assuming x's are distributed evenly on 1..2
-        if ratio < 1:
-            ratio = 1 / ratio
-        index = int((ratio-1) * len(data))
-        return smooth[index]
-
-    # update
-    CHOSEN_RATIO = 0
-    player = sr.PolyphonicPlayer(sr.stream)
-    player.start()
-
-    def update_third_frequency(event):
-        if event.button == 1:
-            choose_second_frequency(event)
-            return
-        player.frequencies = [sr.BASE_FREQUENCY,
-                              CHOSEN_RATIO,
-                              sr.BASE_FREQUENCY * event.xdata]
-
-    def choose_second_frequency(event):
-        global CHOSEN_RATIO
-        CHOSEN_RATIO = event.xdata
-        player.frequencies = [sr.BASE_FREQUENCY,
-                              sr.BASE_FREQUENCY * CHOSEN_RATIO,
-                              sr.BASE_FREQUENCY * event.xdata]
-        harmony_line = [(harmony(x/1)
-                        + harmony(x/CHOSEN_RATIO)
-                        + harmony(CHOSEN_RATIO)) / 3
-                        for x in xs]
-        base_line = [(harmony(x/1)
-                      + harmony(x/1)
-                      + harmony(1)) / 3
-                     for x in xs]
-        ax.cla()
-        ax.plot(xs, base_line, 'blue', linewidth=1)
-        ax.plot(xs, harmony_line, 'red', linewidth=3)
-        redraw()
-
-    fig.canvas.mpl_connect('motion_notify_event', update_third_frequency)
-    fig.canvas.mpl_connect('button_press_event', choose_second_frequency)
-
-    plt.show()
-    sr.stream.stop_stream()
-    sr.stream.close()
-    sr.p.terminate()
+def harmony(ratio):
+    """
+    predict how harmonious given ratio should sound
+    ! assumes x's are distributed evenly on 1..2
+    """
+    if ratio < 1:
+        ratio = 1 / ratio
+    index = int((ratio-1) * len(data))
+    return smooth[index]
 
 
-if __name__ == '__main__':
-    main()
+def choose_second_frequency(event):
+    """
+    choose second note of the chord, and play it,
+    also display in red predicted harmony of a third note, with the other two
+    """
+    global CHOSEN_RATIO
+    CHOSEN_RATIO = event.xdata
+    player.frequencies = [sr.BASE_FREQUENCY,
+                          sr.BASE_FREQUENCY * CHOSEN_RATIO,
+                          sr.BASE_FREQUENCY * event.xdata]
+    harmony_line = [(harmony(x/1)
+                    + harmony(x/CHOSEN_RATIO)
+                    + harmony(CHOSEN_RATIO)) / 3
+                    for x in xs]
+    ax.cla()
+    ax.plot(xs, harmony_line, 'red', linewidth=3)
+    ax.axvline(x=CHOSEN_RATIO, color='red', linewidth=3)
+    draw()
 
 
+def update_third_frequency(event):
+    """
+    choose third note of the chord, and play it
+    """
+    if event.button == 1:
+        choose_second_frequency(event)
+        return
+    player.frequencies = [sr.BASE_FREQUENCY,
+                          sr.BASE_FREQUENCY * CHOSEN_RATIO,
+                          sr.BASE_FREQUENCY * event.xdata]
 
 
+draw()
+CHOSEN_RATIO = 0
+player = sr.PolyphonicPlayer(stream)
+player.start()
 
+fig.canvas.mpl_connect('button_press_event', choose_second_frequency)
+fig.canvas.mpl_connect('motion_notify_event', update_third_frequency)
+
+plt.show()
+stream.stop_stream()
+stream.close()
+p.terminate()
 
 
 
--- survey.py
+++ survey.py
@@ -1,26 +1,45 @@
+#!/usr/bin/env python3
+"""
+plays randomly chosen two notes
+asks user to rate how harmonious they sound
+saves gathered data
+"""
+
 import curses
 import pickle
 import threading
 from time import time, sleep
 
 import numpy as np
-import pyaudio  # sudo apt-get install python-pyaudio
+import pyaudio
+
 
 BIT_RATE = 44000
-BASE_FREQUENCY = 300
+BASE_FREQUENCY = 200
 help_msg = """
 rate how harmonious this sounds on a scale from 1 to 9
 press q to quit
 
 """
 
-p = pyaudio.PyAudio()     # initialize pyaudio
+# credit: stackoverflow.com/questions/10702942/note-synthesis-harmonics-violin-piano-guitar-bass-frequencies-midi
+violin_amps = [0.0, 1.0, 0.286699025, 0.150079537, 0.042909002,
+               0.203797365, 0.229228698, 0.156931925,
+               0.115470898, 0.0, 0.097401803, 0.087653465,
+               0.052331036, 0.052922462, 0.038850593,]
+               # 0.053554676, 0.053697434, 0.022270261,
+               # 0.013072562, 0.008585879, 0.005771505,
+               # 0.004343925, 0.002141371, 0.005343231,
+               # 0.000530244, 0.004711017, 0.009014153]
 
-# for paFloat32 sample values must be in range [-1.0, 1.0]
-stream = p.open(format=pyaudio.paFloat32,
-                channels=1,
-                rate=BIT_RATE,
-                output=True)
+piano_amps = [0.0, 1.0, 0.399064778, 0.229404484, 0.151836061,
+              0.196754229, 0.093742264, 0.060871957,
+              0.138605419, 0.010535002, 0.071021868,]
+              # 0.029954614, 0.051299684, 0.055948288,
+              # 0.066208224, 0.010067391, 0.00753679,
+              # 0.008196947, 0.012955577, 0.007316738,]
+              # 0.006216476, 0.005116215, 0.006243983,
+              # 0.002860679, 0.002558108, 0.0, 0.001650392]
 
 
 def input_char():
@@ -46,7 +65,8 @@ def input_char():
 
 
 class PolyphonicPlayer(threading.Thread):
-    segment_duration = 0.01  # seconds
+    segment_duration = 0.01     # in seconds
+    amps = piano_amps          # tone (amplitude of the overtones)
 
     def __init__(self, stream):
         threading.Thread.__init__(self)
@@ -78,32 +98,56 @@ class PolyphonicPlayer(threading.Thread):
                               .astype(np.float32)
                               .tobytes())
 
-    def get_wave(self, frequency, phase):
-        return np.sin(2 * np.pi *
-                      np.arange(BIT_RATE * self.segment_duration) *
-                      frequency / BIT_RATE
-                      + phase)
+    def get_wave(self, primary_frequency, phase):
+        acc = 0
+        for index, amplitude in enumerate(self.amps):
+            frequency = primary_frequency * index
+            wave = np.sin(2 * np.pi *
+                          np.arange(BIT_RATE * self.segment_duration) *
+                          frequency / BIT_RATE
+                          + phase * index)
+            acc += wave * amplitude
+        # adjust volume
+        maximum_possible_volume = np.sum(self.amps)
+        return acc / maximum_possible_volume
 
     def kill(self):
         self.alive = False
 
 
-if __name__ == '__main__':
-    print('\nchoose nick:')
-    filename = "data/%s.pickle" % input()
+def init():
+    # load data
+    print('choose nick:')
+    filename = "data/%s.pickle" % 'filip'
     try:
         data = pickle.load(open(filename, "rb"))
     except IOError:
         # the file doesn't exist
         data = {}
+    print('data length: %d' % len(data))
 
+    # initialize pyaudio
+    p = pyaudio.PyAudio()
+    # for paFloat32 sample values must be in range [-1.0, 1.0]
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=BIT_RATE,
+                    output=True)
+    return data, p, stream, filename
+
+
+if __name__ == '__main__':
+    data, p, stream, filename = init()
+
+    # survey until interrupted
     try:
         while True:
-            BASE_FREQUENCY = np.random.randint(300, 500)
             ratio = 1 + np.random.random()  # between 1 and 2
 
             player = PolyphonicPlayer(stream)
-            player.frequencies = [BASE_FREQUENCY, BASE_FREQUENCY * ratio]
+            first_frequency = np.random.randint(BASE_FREQUENCY, BASE_FREQUENCY * 2)
+            second_frequency = first_frequency * ratio
+            player.frequencies = [first_frequency, second_frequency]
 
             player.start()          # start playing in a loop
             rating = int(input_char())   # get user rating from 1 to 9
@@ -114,8 +158,6 @@ if __name__ == '__main__':
 
     except:
         # program interrupted
-        # print(e)
-
         pickle.dump(data, open(filename, "wb"))
 
         stream.stop_stream()
